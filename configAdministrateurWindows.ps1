23957a46cef59b733fc1d6cccdb26c3e7445eabf
niszco <nico.marszalek@gmail.com>
2023-08-07 08:34:03 +0200

    Execution Policy by default

--- configAdministrateurWindows.ps1
+++ configAdministrateurWindows.ps1
@@ -1,5 +1,3 @@
-# Permet d'executer le script sur n'importe quelle machine
-Set-ExecutionPolicy Bypass -Scope Process
 # Donne les droits administrateurs pour executer le script
 Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"D:\Script_Prepa_PCs`"" -Verb RunAs
 
